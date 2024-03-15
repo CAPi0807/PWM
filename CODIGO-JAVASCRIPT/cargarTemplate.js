b54a018c7ed427d111b1987b6e663e83750104f9
Javier Sánchez Godoy <81640937+Yaivi@users.noreply.github.com>
2024-03-15 08:02:26 +0000

    conectado js con html

---
+++ CODIGO-JAVASCRIPT/cargarTemplate.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', async function() {
+    await cargarEstructura();
+    // Dado que cargarEstructura es async, ahora puedes asegurarte de que todo ha cargado
+});
+
+async function cargarEstructura() {
+    document.appendChild(await cargarTemplate('../CODIGO-HTML/TEMPLATES/Header/header.html'));
+    document.appendChild(await cargarTemplate('../CODIGO-HTML/TEMPLATES/Footer/footer.html'));
+
+}
+
+async function cargarTemplate(url) {
+    let response = await fetch(url);
+    let text = await response.text();
+
+    let template = document.createElement('template');
+    template.innerHTML = text;
+    return document.importNode(template.content, true);
+}
