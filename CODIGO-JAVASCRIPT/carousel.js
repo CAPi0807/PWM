67a4f9201b831816fe6d74ce2a0439ae31f5a53b
Javier Sánchez Godoy <81640937+Yaivi@users.noreply.github.com>
2024-03-18 10:37:41 +0000

    slider

---
+++ CODIGO-JAVASCRIPT/carousel.js
@@ -0,0 +1,24 @@
+let slideIndex = 1;
+let componentId;
+function showSlides(n) {
+    const slides = document.querySelectorAll(componentId);
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    slides[slideIndex - 1].style.display = 'block';
+}
+
+function moveCarousel(n) {
+    showSlides(slideIndex += n);
+}
+
+function initCarousel(componentID) {
+    componentId = componentID;
+    showSlides(slideIndex);
+}
