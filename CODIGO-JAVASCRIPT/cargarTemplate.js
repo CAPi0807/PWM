5fe96ad2fa246efe33bfda8646622049464437d5
Javier Sánchez Godoy <81640937+Yaivi@users.noreply.github.com>
2024-03-23 19:49:29 +0000

    avances en la carga dinamica

--- CODIGO-JAVASCRIPT/cargarTemplate.js
+++ CODIGO-JAVASCRIPT/cargarTemplate.js
@@ -1,4 +1,19 @@
-function añadirTemplate() {
+function addTemplate(id) {
 
+        let template = id.content.cloneNode();
+        let span = id.querySelector()
     
+}
+function add(url, id) {
+    fetch(url)
+        .then(response => {
+            return response.text();
+        })
+        .then(data => {
+            let container = document.querySelector(id);
+            container.innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error al cargar el template:', error);
+        });
 }
